88d6c7e2ed714cd6cb10859f948520deb059c338
jee00609 <31675804+jee00609@users.noreply.github.com>
2017-11-29 09:38:47 +0900

    두번째
    동적 메모리 할당을 이용한 합 구하기

---
+++ Dynamic2(Sum).c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+	int count, i;
+	int value, sum;
+	int *darray;
+	
+	printf("정수의 개수: ");
+	scanf("%d", &count);
+
+	
+	// Allocate dynamic memory here.
+	darray=(int*)malloc(sizeof(int));
+
+	if( darray == NULL )
+	{
+		printf("동적 메모리 할당 오류");
+		exit(1);
+	}
+	
+	for(i=0;i<count;i++)
+	{
+		printf("양의 정수를 입력하시오: ");
+		scanf("%d", &darray[i]);
+	}
+	
+	sum = 0;
+	
+	for(i=0;i<count;i++)
+	{
+		sum += darray[i];
+	}
+	
+	printf("합은 %d입니다.\n", sum);
+	
+	return 0;
+}
